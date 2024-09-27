9b0a3173db9e28af6ed0d7f2d193378685edeee9
Parneet Singh <parneetsingh@Parneets-MacBook-Pro.local>
2024-09-28 00:52:54 +0200

    Finished lesson 1

--- src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
+++ src/main/scala/com/rockthejvm/part1basics/ValuesAndTypes.scala
@@ -2,28 +2,27 @@ package com.rockthejvm.part1basics
 
 object ValuesAndTypes {
 
-  // values
-  val meaningOfLife: Int = 42 // const int meaningOfLife = 42
+  //values
+  val meaningOfLife: Int = 42     // value cannot be reassigned, assigning is not allowed , constant in other language
 
-  // reassigning is not allowed
-  // meaningOfLife = 45
+  //type inference
+  val anInteger = 67      // :Int is optional ( type inference )
 
-  // type inferrence
-  val anInteger = 67 // : Int is optional
-
-  // common types
+  //common types
   val aBoolean: Boolean = false
   val aChar: Char = 'a'
-  val anInt: Int = 78 // 4 bytes
-  val aShort: Short = 5263 // 2 bytes
-  val aLong: Long = 52789572389234L // 8 bytes
-  val aFloat: Float = 2.4f // 4 bytes
-  val aDouble: Double = 3.14 // 8 bytes
+  val anInt: Int = 78     //4 bytes
+  val aShort: Short = 32    //2 bytes
+  val aLong: Long = 7663L     //8 bytes
+  val aFloat: Float = 7.89f   //4 bytes
+  val aDouble: Double = 3.14    //8 bytes
 
-  // string
+  //String
   val aString: String = "Scala"
 
+  // It will make the object runnable
   def main(args: Array[String]): Unit = {
 
   }
+
 }
